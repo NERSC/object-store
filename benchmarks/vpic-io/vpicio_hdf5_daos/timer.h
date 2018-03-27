2adbd98d19484b217606355fec5c57a776fce9cc
valiantljk <valiantljk@gmail.com>
2018-03-27 13:38:25 -0700

    vpicio hdf5 daos test code

--- benchmarks/vpic-io/vpicio_hdf5_daos/timer.h
+++ benchmarks/vpic-io/vpicio_hdf5_daos/timer.h
@@ -1,3 +1,32 @@
+/****** Copyright Notice ***
+ *
+ * PIOK - Parallel I/O Kernels - VPIC-IO, VORPAL-IO, and GCRM-IO, Copyright
+ * (c) 2015, The Regents of the University of California, through Lawrence
+ * Berkeley National Laboratory (subject to receipt of any required
+ * approvals from the U.S. Dept. of Energy).  All rights reserved.
+ *
+ * If you have questions about your rights to use or distribute this
+ * software, please contact Berkeley Lab's Innovation & Partnerships Office
+ * at  IPO@lbl.gov.
+ *
+ * NOTICE.  This Software was developed under funding from the U.S.
+ * Department of Energy and the U.S. Government consequently retains
+ * certain rights. As such, the U.S. Government has been granted for itself
+ * and others acting on its behalf a paid-up, nonexclusive, irrevocable,
+ * worldwide license in the Software to reproduce, distribute copies to the
+ * public, prepare derivative works, and perform publicly and display
+ * publicly, and to permit other to do so.
+ *
+ ****************************/
+
+/**
+ *
+ * Email questions to SByna@lbl.gov
+ * Scientific Data Management Research Group
+ * Lawrence Berkeley National Laboratory
+ *
+*/
+
 #ifndef __SSI_TIMER_H__
 #define __SSI_TIMER_H__
 
